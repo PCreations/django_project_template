db761eefb871c45ef214cff629df75f5a9e91d27
Pierre Criulanscy <pcriulan@gmail.com>
2015-08-03 19:20:20 +0200

    Added missing replacement for project_name

--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -1,5 +1,5 @@
 """
-Django settings for project_name project.
+Django settings for {{ project_name }} project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -66,9 +66,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'project_name.urls'
+ROOT_URLCONF = '{{ project_name }}.urls'
 
-WSGI_APPLICATION = 'project_name.wsgi.application'
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
